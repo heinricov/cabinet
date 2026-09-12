fff62cc3b24a8ebd280f5d09ae380723e164e0ef
heinricov abraham <heinricoov@gmail.com>
2026-09-12 16:58:09 +0700

    update : 0014

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { backendConfig } from '@workspace/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+  await app.listen(backendConfig.port);
 }
-bootstrap();
+void bootstrap();
